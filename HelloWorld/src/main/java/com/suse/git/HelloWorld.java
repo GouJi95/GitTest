1add8d3e6a4e97486eb8c08adec17c4f750fc4bd
GouJi95 <76900231+GouJi95@users.noreply.github.com>
2021-01-08 11:51:49 +0800

    在线更新

--- HelloWorld/src/main/java/com/suse/git/HelloWorld.java
+++ HelloWorld/src/main/java/com/suse/git/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("create branch");
         System.out.println("分支添加");
         System.out.println("主干添加");
+        System.out.println("在线更新");    
     }
 }

0f0b0da58444e79db6682f5780c321e730125c7b
GouJi95 <1004240192@qq.com>
2021-01-08 11:01:29 +0800

    分支添加

--- HelloWorld/src/main/java/com/suse/git/HelloWorld.java
+++ HelloWorld/src/main/java/com/suse/git/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("create branch");
+        System.out.println("分支添加");
     }
 }
